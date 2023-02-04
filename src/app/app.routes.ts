69a40cb04d6b5f3f12ceae908899fca2c29edc9f
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2023-02-04 11:50:13 +0200

    not-found page edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from 'core/components/not-found';
 
 export const routes: Routes = [
-	
+	{
+		path: '**',
+		component: NotFoundComponent
+	}
 ];
